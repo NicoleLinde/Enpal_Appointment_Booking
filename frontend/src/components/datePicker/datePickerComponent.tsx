6c7a2a6a4074e2b22d922056e000d397b4bf0390
Nicole Linde <>
2024-09-05 12:40:45 +0200

    Add DatePicker component

---
+++ frontend/src/components/datePicker/datePickerComponent.tsx
@@ -0,0 +1,39 @@
+import 'react-datepicker/dist/react-datepicker.css';
+import React, { FunctionComponent, ReactElement } from 'react';
+import RDatePicker, { registerLocale } from 'react-datepicker';
+import { de } from 'date-fns/locale';
+
+import { IDatePickerProps } from './properties';
+
+registerLocale('de', de);
+
+/**
+ * Basic date picker.
+ *
+ * @param {IDatePickerProps} props The component properties.
+ * @returns {ReactElement} The date picker component.
+ */
+const DatePicker: FunctionComponent<IDatePickerProps> = (props): ReactElement => {
+    return (
+        <div className="form-control w-full max-w-full">
+            {props.label && props.label !== '' && <p>{props.label}</p>}
+            <RDatePicker
+                selected={props.date}
+                onChange={(newValue) => {
+                    if (newValue) {
+                        props.onChange(newValue);
+                    }
+                }}
+                locale={'de'}
+                minDate={props.minDate}
+                maxDate={props.maxDate}
+                dateFormat={'dd.MM.yyyy'}
+                placeholderText={props.placeholder}
+                showPopperArrow={false}
+                className={'w-full p-2 border border-gray-300 rounded-md'}
+            />
+        </div>
+    );
+};
+
+export default DatePicker;
